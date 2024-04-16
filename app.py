9dc42df148ddae682bc078bb7d2c0b8c9437286b
c45671234 <c45671234@gmail.com>
2024-04-16 15:38:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 os.system(f"git lfs install")
-os.system(f"git clone https://github.com/comfyanonymous/ComfyUI /home/ubuntu/ui/ComfyUI")
-os.chdir(f"/home/ubuntu/ui/ComfyUI")
+os.system(f"git clone https://github.com/comfyanonymous/ComfyUI /home/demo/source/ComfyUI")
+os.chdir(f"/home/demo/source/ComfyUI")
 os.system(f"pip install -r requirements.txt")
 os.system(f"git clone https://github.com/ltdrdata/ComfyUI-Manager /home/demo/source/ComfyUI/custom_nodes/ComfyUI-Manager")
 os.system(f"python main.py --port 8266 --enable-cors-header")
